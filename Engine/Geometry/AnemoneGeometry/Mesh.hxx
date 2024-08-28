187a50c270a436483075a65e80bc04f666ed178d
selmentdev <selmentdev@gmail.com>
2024-08-28 23:45:32 +0200

    engine: Geometry handling library

---
+++ Engine/Geometry/AnemoneGeometry/Mesh.hxx
@@ -0,0 +1,182 @@
+#pragma once
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+namespace Anemone::Geometry
+{
+    struct FaceIndex
+    {
+        size_t Inner;
+    };
+
+    struct VertexIndex
+    {
+        size_t Inner;
+    };
+
+    struct WedgeIndex
+    {
+        size_t Inner;
+    };
+
+    struct CornerIndex
+    {
+        size_t Inner;
+    };
+
+    /// - all faces are triangles
+    /// - mesh face has N corners (N=3 for triangles).
+    /// - wedge all attributes of each corner of face
+    ///   - wedge_id = face_id * corners + corner_id
+    /// - vertex - vertex position shared by wedges
+    class GEOMETRY_API Mesh final
+    {
+    public:
+        static constexpr size_t MaxTextureCoords = 8;
+        static constexpr size_t FacePrimitiveCornersCount = 3;
+
+        // FaceMaterialIndex[face_index] = material_index.
+        std::vector<int32_t> FaceMaterialIndices;
+
+        // FaceSmoothingMasks[face_index] = smooth_mask.
+        std::vector<uint32_t> FaceSmoothingMasks;
+
+        // VertexPositions[vertex_index] = Float3(position)
+        std::vector<Float3> VertexPositions;
+
+        // WedgeIndices[wedge_index] = vertex_index
+        std::vector<uint32_t> WedgeIndices;
+
+        // WedgeTangentX[wedge_index] = tangent_x
+        std::vector<Float3> WedgeTangentX;
+
+        // WedgeTangentY[wedge_index] = tangent_y
+        std::vector<Float3> WedgeTangentY;
+
+        // WedgeTangentZ[wedge_index] = tangent_z
+        std::vector<Float3> WedgeTangentZ;
+
+        // WedgeTextureCoords[uv_layer][wedge_index] = texcoords.
+        std::vector<Float2> WedgeTextureCoords[MaxTextureCoords];
+
+        // WedgeColors[wedge_index] = color.
+        std::vector<Float4> WedgeColors;
+
+    public:
+        constexpr size_t GetVerticesCount() const noexcept
+        {
+            return static_cast<uint32_t>(this->VertexPositions.size());
+        }
+
+        constexpr size_t GetWedgesCount() const
+        {
+            return this->WedgeIndices.size();
+        }
+
+        constexpr size_t GetFacesCount() const
+        {
+            return this->WedgeIndices.size() / FacePrimitiveCornersCount;
+        }
+
+        static constexpr WedgeIndex ComputeWedgeIndex(FaceIndex face, CornerIndex corner)
+        {
+            return WedgeIndex{face.Inner * FacePrimitiveCornersCount + corner.Inner};
+        }
+
+        static constexpr FaceIndex ComputeFaceIndex(CornerIndex corner_index)
+        {
+            return FaceIndex{corner_index.Inner / FacePrimitiveCornersCount};
+        }
+    };
+}
+
+namespace Anemone::Geometry::EditableMesh
+{
+    struct VertexId final
+    {
+        uint32_t Inner;
+
+        static constexpr VertexId Invalid() { return {~static_cast<uint32_t>(0)}; }
+    };
+
+    struct VertexInstanceId final
+    {
+        uint32_t Inner;
+
+        static constexpr VertexInstanceId Invalid() { return {~static_cast<uint32_t>(0)}; }
+    };
+
+    struct EdgeId final
+    {
+        uint32_t Inner;
+
+        static constexpr EdgeId Invalid() { return {~static_cast<uint32_t>(0)}; }
+    };
+
+    struct PolygonGroupId final
+    {
+        uint32_t Inner;
+
+        static constexpr PolygonGroupId Invalid() { return {~static_cast<uint32_t>(0)}; }
+    };
+
+    struct PolygonId final
+    {
+        uint32_t Inner;
+
+        static constexpr PolygonId Invalid() { return {~static_cast<uint32_t>(0)}; }
+    };
+
+    struct MeshVertex
+    {
+        std::vector<VertexInstanceId> Instances;
+        std::vector<EdgeId> ConnectedEdges;
+    };
+
+    struct MeshVertexInstance
+    {
+        VertexId Vertex;
+        std::vector<PolygonId> ConnectedPolygons;
+    };
+
+    struct MeshEdge
+    {
+        VertexId Vertices[2];
+        std::vector<PolygonId> ConnectedPolygons;
+    };
+
+    struct MeshPolygonContour
+    {
+        std::vector<VertexInstanceId> Instances;
+    };
+
+    struct MeshTriangle
+    {
+        VertexInstanceId Vertex1;
+        VertexInstanceId Vertex2;
+        VertexInstanceId Vertex3;
+    };
+
+    struct MeshPolygon
+    {
+        MeshPolygonContour Perimeter;
+        std::vector<MeshPolygonContour> Holes;
+        std::vector<MeshTriangle> Triangles;
+        PolygonGroupId GroupId;
+    };
+
+    struct MeshPolygonGroup
+    {
+        std::vector<PolygonId> Polygons;
+    };
+
+    struct MeshDescriptor
+    {
+        std::vector<MeshVertex> Vertices;
+        std::vector<MeshVertexInstance> VertexInstancesCollection;
+        std::vector<MeshEdge> EdgesCollection;
+        std::vector<MeshPolygon> PolygonsCollection;
+        std::vector<MeshPolygonGroup> PolygonGroupsCollection;
+    };
+}
