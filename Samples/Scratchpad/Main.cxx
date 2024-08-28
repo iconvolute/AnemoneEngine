bbc3b92b354bb784431f4ea67f406a9a73b85048
selmentdev <selmentdev@gmail.com>
2024-08-28 23:47:12 +0200

    samples: scratch pad exe

--- Samples/Scratchpad/Main.cxx
+++ Samples/Scratchpad/Main.cxx
@@ -1,10 +1,43 @@
 #include "AnemoneRuntime/Instant.hxx"
 #include "AnemoneRuntime/Platform/Main.hxx"
+#include "AnemoneRuntime/Profiler/Profiler.hxx"
+#include "AnemoneRuntime/Threading/Yielding.hxx"
+#include "AnemoneRuntime/Platform/Runtime.hxx"
 
 #include <fmt/format.h>
 
-int main()
+
+AE_DECLARE_PROFILE(Main);
+AE_DECLARE_PROFILE(Outer);
+AE_DECLARE_PROFILE(Inner);
+
+int main(int argc, char* argv[])
 {
-    fmt::println("Hello at {}", Anemone::Instant::Now().SinceEpoch());
+    Anemone::Platform::Runtime runtime{argc, argv};
+
+    {
+        AE_PROFILE_SCOPE(Main);
+        {
+            AE_PROFILE_SCOPE(Inner);
+            fmt::println("Hello at {}", Anemone::Instant::Now().SinceEpoch());
+        }
+
+        Anemone::Threading::ThisThread::Sleep(Anemone::Duration::FromMilliseconds(500));
+        {
+            AE_PROFILE_SCOPE(Outer);
+            Anemone::Threading::ThisThread::Sleep(Anemone::Duration::FromMilliseconds(500));
+            {
+                AE_PROFILE_SCOPE(Inner);
+                Anemone::Threading::ThisThread::Sleep(Anemone::Duration::FromMilliseconds(500));
+
+                volatile int* p = (int*)0x4242424242424242;
+                *p = 42;
+
+                fmt::println("Crashed!");
+            }
+            Anemone::Threading::ThisThread::Sleep(Anemone::Duration::FromMilliseconds(500));
+        }
+        Anemone::Threading::ThisThread::Sleep(Anemone::Duration::FromMilliseconds(500));
+    }
     return 0;
 }
