4b280aea26ecc2345277658c8953d59ec7a1c7d8
selmentdev <selmentdev@gmail.com>
2025-04-01 19:53:09 +0200

    runtime: simplified Parallel For implementation

--- Engine/Runtime/AnemoneRuntime/Tasks/Parallel.hxx
+++ Engine/Runtime/AnemoneRuntime/Tasks/Parallel.hxx
@@ -1,9 +1,8 @@
 #pragma once
-#include <algorithm>
-
 #include "AnemoneRuntime/Platform/Base/BaseHeaders.hxx"
 #include "AnemoneRuntime/Tasks/TaskScheduler.hxx"
 #include "AnemoneRuntime/Diagnostics/Assert.hxx"
+#include "AnemoneRuntime/FunctionRef.hxx"
 
 namespace Anemone
 {
@@ -11,154 +10,64 @@ namespace Anemone
     {
         Parallel() = delete;
 
-        template <typename Callback = void(size_t index, size_t count), typename Finalize = void(size_t count)>
+        RUNTIME_API static void For(
+            size_t count,
+            size_t batch,
+            FunctionRef<void(size_t index, size_t count)> callback,
+            FunctionRef<void(size_t count)> finalize,
+            size_t workers = 0,
+            TaskPriority priority = TaskPriority::Inherited);
+
         static void For(
             size_t count,
             size_t batch,
-            Callback&& callback,
-            Finalize&& finalize,
+            FunctionRef<void(size_t index, size_t count)> callback,
             size_t workers = 0,
             TaskPriority priority = TaskPriority::Inherited)
         {
-            AE_ASSERT(batch != 0);
-            if (count == 0)
+            For(count, batch, callback, [](size_t)
             {
-                //
-                // Make sure to call the finalize callback when the count is zero.
-                //
-
-                finalize(count);
-                return;
-            }
-
-            class Partitioner final
-            {
-            public:
-                size_t m_Count;
-                size_t m_Batch;
-                std::atomic<size_t> m_Index{};
-                Callback& m_Callback;
-
-            public:
-                Partitioner(
-                    size_t count,
-                    size_t batch,
-                    Callback& callback)
-                    : m_Count{count}
-                    , m_Batch{batch}
-                    , m_Callback{callback}
-                {
-                }
-                Partitioner(Partitioner const&) = delete;
-                Partitioner(Partitioner&&) = delete;
-                Partitioner& operator=(Partitioner const&) = delete;
-                Partitioner& operator=(Partitioner&&) = delete;
-                ~Partitioner() = default;
-
-                bool Partition(size_t& first, size_t& last)
-                {
-                    size_t const current = this->m_Index.fetch_add(this->m_Batch, std::memory_order::acquire);
-
-                    if (current < this->m_Count)
-                    {
-                        first = current;
-                        last = std::min<size_t>(current + this->m_Batch, this->m_Count);
-                        return true;
-                    }
-
-                    return false;
-                }
-
-                void ExecuteCallbacks()
-                {
-                    size_t first;
-                    size_t last;
-
-                    while (this->Partition(first, last))
-                    {
-                        this->m_Callback(first, last - first);
-                    }
-                }
-            };
-
-            class ParallelForTask final : public Task
-            {
-            private:
-                Partitioner& m_Partitioner;
-
-            public:
-                explicit ParallelForTask(Partitioner& parent)
-                    : m_Partitioner{parent}
-                {
-                }
-
-                ParallelForTask(ParallelForTask const&) = delete;
-                ParallelForTask(ParallelForTask&&) = delete;
-                ParallelForTask& operator=(ParallelForTask const&) = delete;
-                ParallelForTask& operator=(ParallelForTask&&) = delete;
-                ~ParallelForTask() override = default;
-
-            protected:
-                void OnExecute() override
-                {
-                    this->m_Partitioner.ExecuteCallbacks();
-                }
-            };
-
-            // Number of worker threads in pool.
-            auto workersCount = std::max<size_t>(1, TaskScheduler::GetWorkerCount());
-
-            if (workers == 0)
-            {
-                // Caller did not specify the number of threads.
-                workers = workersCount;
-            }
-            else
-            {
-                // Adjust the number of threads to the number of workers.
-                workers = std::min<size_t>(workers, workersCount);
-            }
-
-            // Estimated number of slices.
-            size_t const slices = (count / batch) + 1;
-
-            // TODO: Promote this to some configurable constant.
-            constexpr auto maximumThreads = 32uz;
-
-            // Adjust the number of threads to the number of slices.
-            workers = std::min<size_t>({workers, slices, maximumThreads});
-
-            // Spawn tasks to (hopefully) saturate thread pool.
-            TaskHandle children[32];
-
-            // Create partitioner for given range.
-            Partitioner partitioner{
-                count,
-                batch,
-                callback,
-            };
-
-            // Fork/Join awaiter.
-            AwaiterHandle joinCounter = new Awaiter{};
-            AwaiterHandle forkCounter = new Awaiter{};
-
-            // Spawn tasks.
-            for (auto i = 0uz; i < workers; ++i)
-            {
-                children[i] = new ParallelForTask(partitioner);
-
-                TaskScheduler::Dispatch(*children[i], joinCounter, forkCounter, priority);
-            }
-
-            // Contribute with executing tasks.
-            partitioner.ExecuteCallbacks();
-
-            // Wait for counter.
-            // TODO: for diagnostic purposes, could we return number of processed tasks during awaiting?
-            TaskScheduler::Wait(joinCounter);
-
-            // Finalize range.
-            std::forward<Finalize>(finalize)(count);
+            }, workers, priority);
         }
     };
+}
+
+// New design on tasks public API.
+namespace Anemone
+{
+    class IParallelTask
+    {
+    public:
+        IParallelTask() = default;
+        IParallelTask(IParallelTask const&) = default;
+        IParallelTask(IParallelTask&&) = default;
+        IParallelTask& operator=(IParallelTask const&) = default;
+        IParallelTask& operator=(IParallelTask&&) = default;
+        virtual ~IParallelTask() = default;
+
+    public:
+        virtual void OnExecute(size_t index, size_t count) = 0;
+        virtual void OnFinalize(size_t count) = 0;
+
+        AwaiterHandle Schedule(size_t index, size_t count);
+        AwaiterHandle Schedule(size_t index, size_t count, AwaiterHandle const& dependency);
+    };
+
+    class ITask
+    {
+    public:
+        ITask() = default;
+        ITask(ITask const&) = default;
+        ITask(ITask&&) = default;
+        ITask& operator=(ITask const&) = default;
+        ITask& operator=(ITask&&) = default;
+        virtual ~ITask() = default;
+
+    public:
+        virtual void OnExecute() = 0;
+
+        AwaiterHandle Schedule();
+        AwaiterHandle Schedule(AwaiterHandle const& dependency);
+    };
+
 }
