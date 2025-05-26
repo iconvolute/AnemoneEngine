ea74dfb4e4de92f442cebf51e66274cbc6603de3
selmentdev <selmentdev@gmail.com>
2025-05-26 17:08:19 +0200

    runtime: getting windows sdk from registry

---
+++ Engine/Runtime/AnemoneRuntime/Interop/Windows/Sdk.hxx
@@ -0,0 +1,27 @@
+#pragma once
+#include "AnemoneRuntime/Interop/Windows/Registry.hxx"
+
+namespace Anemone::Interop::Windows
+{
+    template <typename Char, size_t Capacity>
+    bool GetWindowsSdkVersion(string_buffer<Char, Capacity>& result)
+    {
+        if (RegistryKey key = RegistryKey::Open(HKEY_LOCAL_MACHINE, LR"(SOFTWARE\Microsoft\Microsoft SDKs\Windows\v10.0)"))
+        {
+            return key.ReadString(L"ProductVersion", result);
+        }
+
+        return false;
+    }
+
+    template <typename Char, size_t Capacity>
+    bool GetWindowsSdkLocation(string_buffer<Char, Capacity>& result)
+    {
+        if (RegistryKey key = RegistryKey::Open(HKEY_LOCAL_MACHINE, LR"(SOFTWARE\Microsoft\Microsoft SDKs\Windows\v10.0)"))
+        {
+            return key.ReadString(L"InstallationFolder", result);
+        }
+
+        return false;
+    }
+}
