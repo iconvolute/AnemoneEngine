0bf1e5e93938d96ee7a359be12245c826504d9cd
selmentdev <selmentdev@gmail.com>
2025-10-04 12:27:51 +0200

    runtime: file trace listener

---
+++ Engine/Runtime/AnemoneRuntime/Diagnostics/Internal/FileTraceListener.hxx
@@ -0,0 +1,20 @@
+#pragma once
+#include "AnemoneRuntime/Diagnostics/TraceListener.hxx"
+#include "AnemoneRuntime/Storage/FileOutputStream.hxx"
+
+namespace Anemone
+{
+    class RUNTIME_API FileTraceListener final : public TraceListener
+    {
+    private:
+        ReaderWriterLock _lock{};
+        Reference<FileOutputStream> _stream{};
+
+    public:
+        explicit FileTraceListener(Reference<FileOutputStream> stream);
+
+    public:
+        void TraceEvent(TraceLevel level, const char* message, size_t size) override;
+        void Flush() override;
+    };
+}
